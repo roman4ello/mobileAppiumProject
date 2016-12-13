efba2e53e849516b154e6c35c91855458a240938
Boyar Roman <br777roman@gmail.com>
2016-12-13 11:00:32 +0200

    first commit

---
+++ src/test/java/scenarios/AndroidSetup.java
@@ -0,0 +1,24 @@
+package scenarios;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.io.File;
+import java.net.URL;
+
+
+public class AndroidSetup {
+    protected AndroidDriver driver;
+
+    protected void prepareAndroidForAppium() throws MalformedURLException {
+        File appDir = new File("/myAppiumProject/apps");
+        File app = new File(appDir, "linkedin-4-0-93.apk");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("device","Android");
+
+
+        capabilities.setCapability("app", app.getAbsolutePath());
+        driver =  new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+}
